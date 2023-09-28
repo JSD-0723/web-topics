0b987e71f23965b0ddfae9755d0a1aa6a162eeb0
Hatem Ratrout <76398557+HatemRatrout@users.noreply.github.com>
2023-09-28 16:19:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   
   return (
     <ThemeContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<TopicsDisplay />} />
