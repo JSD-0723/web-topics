c12631a925b950b8f390d83fc712871c2582e135
Hatem Ratrout <76398557+HatemRatrout@users.noreply.github.com>
2023-09-28 18:00:11 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import PrimaryHeader from './PrimaryHeader';
 import { MainContainer } from "./MainContainer";
 import Footer from './Footer';
 import { useThemeContext } from "./ThemeContext";
+import SecondaryHeader from "./SecondaryHeader";
 
 
 export const Layout = () => {
@@ -10,6 +11,7 @@ export const Layout = () => {
     console.log("Theme", theme);
     return <>
         <PrimaryHeader />
+        <SecondaryHeader/>
         <MainContainer>
             <main>
                 <Outlet />
